e6ec2f4fd226e4c2b7a36d4749ef33b69ef04387
Destin Lee <dleeinc@gmail.com>
2025-01-09 11:43:55 -0600

    moving some terminal stuff around

--- lua/destinio.lua
+++ lua/destinio.lua
@@ -1,6 +1,22 @@
 local M = {}
 
-M.setup = function() end
+M.setup = function()
+  vim.keymap.set("n", "<Leader>tt", M.right_terminal, { desc = "right Terminal" })
+
+  -- User Commands
+  vim.api.nvim_create_user_command("TermOpen", function()
+    vim.cmd("split | terminal")
+  end, {})
+
+  -- Autocommands
+  vim.api.nvim_create_autocmd("TermOpen", {
+    callback = function()
+      vim.wo.number = false -- Disable line numbers in terminal
+      vim.wo.relativenumber = false
+      vim.cmd("startinsert") -- Start in insert mode
+    end,
+  })
+end
 
 M.say_hello = function()
   vim.print("Hello from destinio.nvim")
@@ -8,6 +24,15 @@ end
 
 M.rtps = function()
   vim.print(vim.inspect(vim.api.nvim_list_runtime_paths()))
+end
+
+-- Terminal Stuff
+function M.right_terminal(opts)
+  opts = opts or {}
+
+  local width = math.floor(vim.o.columns * (opts.width or 0.4))
+
+  vim.cmd(width .. "vsplit | terminal")
 end
 
 return M
